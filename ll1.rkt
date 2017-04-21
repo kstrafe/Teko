31f6aff20e12360b706859ba0d275a45bcd9fd71
Kevin Robert Stravers <macocio@gmail.com>
2017-04-21 20:09:35 +0200

    Fix edge cases

--- ll1.rkt
+++ ll1.rkt
@@ -5,43 +5,49 @@
 
 (define prepend cons)
 
-(struct/lens state (tree list token) #:prefab)
-(define empty-state (state empty #f ""))
+(struct/lens state (token stack program) #:prefab)
+(define empty-state (state "" #f empty))
 
 (define (append-character-to-token character state)
   (lens-transform state-token-lens state (curryr string-append (string character))))
 
-(define (move-list-to-tree state)
+(define (move-stack-to-program state)
   (~>
-    (lens-transform state-tree-lens state (lambda (x) (cons (state-list state) x)))
-    (lens-set state-list-lens _ #f)))
+    (lens-transform state-program-lens state (lambda (x) (cons (first (state-stack state)) x)))
+    (lens-set state-stack-lens _ #f)))
 
 (define (interpret-left-parenthesis state)
-  (cond
-    ([false? (state-list state)] (lens-set       state-list-lens state '(())))
-    ([list?  (state-list state)] (lens-transform state-list-lens state (curry cons empty)))
-    (else "Something is seriously wrong, state-list is neither false nor a list")))
-(define (interpret-right-parenthesis state)
-  (let ([state* (move-token-to-list state)])
+  (let ([state* (move-token-to-stack state)])
     (cond
-      ([= (length (state-list state*)) 1] (move-list-to-tree state*))
-      (else (lens-transform state-list-lens state* (lambda (x) (cons (cons (first x) (second x)) (rest (rest x)))))))))
+      ([false? (state-stack state*)] (lens-set       state-stack-lens state* '(())))
+      ([list? (state-stack state*)]  (lens-transform state-stack-lens state* (curry cons empty)))
+      (else "Something is seriously wrong, state-stack is neither false nor a stack"))))
+
+(define (interpret-right-parenthesis state)
+  (let ([state* (move-token-to-stack state)])
+    (cond
+      ([= (length (state-stack state*)) 1] (move-stack-to-program state*))
+      (else (lens-transform state-stack-lens state* (lambda (x) (cons (cons (first x) (second x)) (rest (rest x)))))))))
 
 (define (clear-token state) (lens-set state-token-lens state ""))
-(define (clear-list  state) (lens-set state-list-lens  state empty))
+(define (clear-stack  state) (lens-set state-stack-lens  state empty))
 
-(define (move-token-to-list state)
+(define (move-token-to-stack state)
   (if (non-empty-string? (state-token state))
-    (~>
-      (lens-transform (lens-compose first-lens state-list-lens) state (curry prepend (state-token state)))
-      clear-token)
+    (if (false? (state-stack state))
+      (~>
+        (lens-transform (lens-compose state-program-lens) state (curry prepend (state-token state)))
+        clear-token)
+      (~>
+        (lens-transform (lens-compose first-lens state-stack-lens) state (curry prepend (state-token state)))
+        clear-token))
     state))
 
 (let ([string (string->list (file->string "input"))])
   (define (ll1 in state)
-    (displayln state)
+    (writeln state)
     (cond
-      ([char-whitespace? in] (move-token-to-list state))
+      ([char-whitespace? in] (move-token-to-stack state))
       ([char=? in #\(] (interpret-left-parenthesis state))
       ([char=? in #\)] (interpret-right-parenthesis state))
       (else (append-character-to-token in state))))
