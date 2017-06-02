aba0f4b11f09469e190f35e145a2364459f0e209
Kevin Robert Stravers <macocio@gmail.com>
2017-06-02 06:08:54 +0200

    Fix up the error states

--- Teko/src/lib.rs
+++ Teko/src/lib.rs
@@ -1,17 +1,16 @@
 extern crate num;
 
 use std::fs::File;
-use std::io::prelude::*;
 use std::io::Read;
 
 #[derive(Debug)]
-enum List {
+pub enum List {
 	Node(Vec<List>),
 	Leaf(String),
 }
 
 #[derive(Clone, Copy, Debug)]
-struct Position {
+pub struct Position {
 	line: usize,
 	column: usize,
 }
@@ -26,7 +25,7 @@ impl Default for Position {
 }
 
 #[derive(Debug)]
-struct State {
+pub struct State {
 	position: Position,
 	unmatched_opening_parentheses: Vec<Position>,
 	token: String,
@@ -48,37 +47,51 @@ impl Default for State {
 	}
 }
 
-fn parse_file(filename: &str) -> State {
+pub fn parse_file(filename: &str) -> Result<List, State> {
 	let mut file = File::open(filename).ok().unwrap();
 	let mut contents = String::new();
 	file.read_to_string(&mut contents).ok();
 	parse_string(&contents)
 }
 
-fn parse_string(string: &str) -> State {
+////////////////////////////////////////////////////////////
+
+pub fn parse_string(string: &str) -> Result<List, State> {
 	let mut state = State::default();
 	for character in string.chars() {
-		println!["{:#?}", state];
+		// println!["{:#?}", state];
 		parse_character(character, &mut state);
 		if state.error.is_some() {
 			break;
 		}
 	}
-	finish_parsing_characters(&mut state);
-	state
+	println!("{:#?}", state);
+	finish_parsing_characters(state)
 }
 
 ////////////////////////////////////////////////////////////
 
-fn finish_parsing_characters(state: &mut State) {
-	whitespace(state);
+pub fn finish_parsing_characters(mut state: State) -> Result<List, State> {
+	whitespace(&mut state);
+		println!("{:#?}", state);
+	if ! state.stack.is_empty() {
+		println!("{:#?}", state);
+		set_error(&mut state, "Unmatched opening parenthesis");
+		Err(state)
+	} else if state.error.is_some() {
+		Err(state)
+	} else {
+		Ok(List::Node(state.program))
+	}
 }
 
-fn parse_character(character: char, state: &mut State) {
+pub fn parse_character(character: char, state: &mut State) {
 	parse_internal(character, state);
 	count_characters_and_lines(character, state);
 }
 
+////////////////////////////////////////////////////////////
+// Internal                                               //
 ////////////////////////////////////////////////////////////
 
 fn count_characters_and_lines(character: char, state: &mut State) {
@@ -128,6 +141,7 @@ fn left_parenthesis(state: &mut State) {
 fn right_parenthesis(state: &mut State) {
 	whitespace(state);
 	pop_previous_opening_parenthesis(state);
+	if state.error.is_some() { return; }
 	if state.stack.len() == 1 {
 		move_stack_to_program(state);
 	} else if state.stack.len() >= 2 {
@@ -154,12 +168,21 @@ fn move_stack_to_program(state: &mut State) {
 	state.program.push(state.stack.pop().unwrap());
 }
 
+fn set_error(state: &mut State, message: &str) {
+	state.token.clear();
+	state.stack.clear();
+	state.program.clear();
+	state.error = Some(String::from(message));
+}
+
 fn copy_location_to_last_open_location(state: &mut State) {
 	state.unmatched_opening_parentheses.push(state.position);
 }
 
 fn pop_previous_opening_parenthesis(state: &mut State) {
-	state.unmatched_opening_parentheses.pop();
+	if ! state.unmatched_opening_parentheses.pop().is_some() {
+		set_error(state, "Unmatched closing parenthesis");
+	}
 }
 
 ////////////////////////////////////////////////////////////
@@ -169,6 +192,6 @@ mod tests {
 	use super::*;
 	#[test]
 	fn it_works() {
-		println!["{:#?}", parse_string("(+ 1 2 3 (* 4 5) ) +")];
+		println!["{:#?}", parse_string("(+ 1 2 3 (* 4 5) )+ (define (fn (x) (+ x y)))")];
 	}
 }
